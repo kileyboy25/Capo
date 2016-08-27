ddbeb5f49e2f7033922c64e968c6aec592b9ae50
Mayur Argekar <mayur.arge@gmail.com>
2016-08-27 00:53:07 -0500

    Add source files and basic UI for app. NOTE: App.iml was changed here.

---
+++ app/src/main/java/com/broadgames/capo/HomeScreenActivity.java
@@ -0,0 +1,25 @@
+package com.broadgames.capo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+        Button loginButton = (Button) findViewById(R.id.home_screen_login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
