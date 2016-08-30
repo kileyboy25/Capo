47c074c2119dad6a02c40a85ab5fcf4faba798a3
Mayur Argekar <mayur.arge@gmail.com>
2016-08-29 23:44:40 -0500

    Add request and propose ride screens.

---
+++ app/src/main/java/com/broadgames/capo/RequestRideActivity.java
@@ -0,0 +1,13 @@
+package com.broadgames.capo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RequestRideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_ride);
+    }
+}
